00dc3e057861c1e146a34f113d0127f82e2ac530
Nikita Tran <nikita.tran.nt@gmail.com>
2024-04-11 05:17:36 -0400

    Add output target; Re-add nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  site: 'https://nikitatran.github.io',
-  base: "/astro-simple-resume"
+  output: "static",
+  site: "https://nikitatran.github.io",
+  base: "/astro-simple-resume",
+  integrations: [tailwind(), icon()]
 });
