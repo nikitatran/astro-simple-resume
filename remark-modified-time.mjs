87c5a8399a32e882969c55f9a7264436535ff6f1
Nikita Tran <nikita.tran.nt@gmail.com>
2024-04-12 13:58:16 -0400

    Remove test logs in remark function; move last modified time from layout to footer component

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -4,8 +4,6 @@ export function remarkModifiedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
     const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
-    console.log("TEST_FILEPATH: ", filepath.toString());
     file.data.astro.frontmatter.lastModified = result.toString();
-    console.log("TEST_FILEPATH: ", result.toString());
   };
 }
